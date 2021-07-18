42edaafb374ecff9621d940c75f164ed6e795671
linh442k <nhatlinh4459@gmail.com>
2021-07-18 23:40:08 +0700

    todolist with useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import uuid from "uuid/v4";
 
-function App() {
+const initialTodos = [
+  {
+    id: uuid(),
+    task: "Learn React",
+    complete: true,
+  },
+  {
+    id: uuid(),
+    task: "Learn Firebase",
+    complete: true,
+  },
+  {
+    id: uuid(),
+    task: "Learn GraphQL",
+    complete: false,
+  },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
+  const [task, setTask] = useState("");
+
+  const handleChangeInput = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    if (task) {
+      // add new todo item
+      setTodos(todos.concat({ id: uuid(), task, complete: false }));
+    }
+
+    setTask("");
+
+    event.preventDefault();
+  };
+
+  const handleChangeCheckBox = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label
+              style={todo.complete ? { textDecoration: "line-through" } : {}}
+            >
+              <input
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => handleChangeCheckBox(todo.id)}
+              />
+              {todo.task}
+            </label>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={task} onChange={handleChangeInput} />
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
